894dd0e3a92e11ba8c9efa4b135494bc300709ef
mallemouche christelle <christellema08@gmail.com>
2020-01-31 14:24:08 +0100

    Add first route and Component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
+    <FirstComponent/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/test">Test</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import FirstComponent from './components/FirstComponent'
+export default {
+  name: 'App',
+  components: {
+   FirstComponent
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+    <div>
+        <h1> Tritre 1 </h1>
+        <h2> Titre 2 </h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style></style>
+
