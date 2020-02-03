27619de9c3b2e2a4fc6091798258efc7740ef124
mallemouche christelle <christellema08@gmail.com>
2020-02-03 17:00:50 +0100

    navbar et carousel boostrapvue

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,66 @@
 <template>
   <div id="app">
-    <!-- <FirstComponent/>
+    <Navbar/>
+    <Carousel/>
+    <FirstComponent/>
     <Second/>
     <Third/>
     <Fourth/>
-    <Firth/> -->
-    <Navbar/>
+    <Firth/>
+    <ButtonAlert/>
     <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <router-link to="/test">Test</router-link> -->
+      <router-link to="/test">Test</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-
-import Navbar from './components/Navbar'
-
+//css
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 // app.js
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+
+////////A VOIR ?????
+import { BNavbar } from 'bootstrap-vue'
+Vue.component('b-navbar', BNavbar)
+
+import { NavbarPlugin } from 'bootstrap-vue'
+Vue.use(NavbarPlugin)
+//////////A VOIR ????
+
+
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
-
-
-// import FirstComponent from './components/FirstComponent'
-// import Second from './components/Second'
-// import Third from './components/Third'
-// import Fourth from './components/Fourth'
-// import Firth from './components/Firth'
+//import components
+import FirstComponent from './components/FirstComponent'
+import Second from './components/Second'
+import Third from './components/Third'
+import Fourth from './components/Fourth'
+import Firth from './components/Firth'
+import ButtonAlert from './components/ButtonAlert'
+import Navbar from './components/Navbar'
+import Carousel from './components/Carousel'
 
 export default {
   name: 'App',
   components: {
-    // FirstComponent,
-    // Second,
-    // Third,
-    // Fourth,
-    // Firth,
-    Navbar
+    FirstComponent,
+    Second,
+    Third,
+    Fourth,
+    Firth,
+    ButtonAlert,
+    Navbar, 
+    Carousel
   }
 }
 </script>
---
+++ src/components/ButtonAlert.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <b-button size="sm" @click="toggle">
+    {{ show ? 'Hide' : 'Show' }} Alert
+  </b-button>
+  <b-alert
+    v-model="show"
+    class="mt-3"
+    dismissible
+    @dismissed="dismissed"
+  >
+    Hello {{ name }}!
+  </b-alert>
+</div>
+</template>
+
+
+<script>
+export default {
+    
+    data() {
+        return {
+            name: 'le monde',
+            show: true
+        }
+    },
+
+    watch() {
+        return {
+            show(newVal) {
+                'Alert is now ' + (newVal ? 'visible' : 'hidden')
+            }
+        }
+    },
+
+    methods() {
+        return {
+            toggle() {
+                'Toggle button clicked'
+                this.show = !this.show
+            },
+
+            dismissed() {
+                'Alert dismissed'
+            }
+        }
+    }
+}
+</script>
